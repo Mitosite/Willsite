2c271893cca1937477da69f024f7465a0023edd4
whbtbowers <33463737+whbtbowers@users.noreply.github.com>
2018-02-19 12:13:54 +0000

    Add files via upload

--- mitosite/align/forms.py
+++ mitosite/align/forms.py
@@ -1,15 +1,24 @@
 from django import forms
-from align.models import UploadFile, Pairedjob
+
 
 
 class UploadFileForm(forms.Form):
 	title = forms.CharField(label='Your name', max_length=100)
-	#class Meta:
-		#model = UploadFile
-		#fields = ('title', 'file')
+	file = forms.FileField()
+	user_email = forms.EmailField(required=False)
+
+class SingleJobForm(forms.Form):
+	#form to submit single job
+	readsfile = forms.FileField(label='Reads file:')
+	#adapters = forms.CharField(label='Adapter sequence:', max_length=50)
+	#key = forms.CharField(label='Random key:', max_length=10)
+	user_email = forms.EmailField(required=False)
 
 class PairedJobForm(forms.Form):
-	readsfile1 = forms.FileField()
+	readsfile1 = forms.FileField(label='First reads file:')
+	readsfile2 = forms.FileField(label='Second reads file:')
+	adapters = forms.CharField(label='Adapter sequence:', max_length=50)
+	key = forms.CharField(label='Random key:', max_length=10)
 
 
 '''
